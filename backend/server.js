136466c54d7a1adea7106958f27324bd69dd0556
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-11-21 02:54:18 +0530

    Added path in server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from 'path';
 import userRouter from "./routers/userRouters.js";
 import productRouter from "./routers/productRouter.js";
 import orderRouter from "./routers/orderRouter.js";
@@ -14,12 +15,17 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazonMERN", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const __dirname = path.resolve();
 app.use("/api/users", userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
